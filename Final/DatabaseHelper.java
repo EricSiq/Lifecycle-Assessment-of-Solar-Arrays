dd48fe7f3196cbcbca8165400b25a2d7880f0e0f
Eric Siqueira <eric.siqueira005@gmail.com>
2025-04-01 10:07:54 +0530

    Final codes

---
+++ Final/DatabaseHelper.java
@@ -0,0 +1,79 @@
+import java.sql.*;
+
+public class DatabaseHelper {
+    private static final String URL = "jdbc:mysql://localhost:3306/PIJ_Project_Database";
+    private static final String USER = "root";
+    private static final String PASSWORD = "XlR8#2137";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("MySQL JDBC Driver not found! Add the connector JAR.", e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static ResultSet getProjects() throws SQLException {
+        return executeQuery("SELECT * FROM Projects");
+    }
+
+    public static ResultSet getPowerPurchaseAgreements() throws SQLException {
+        return executeQuery("SELECT * FROM PowerPurchaseAgreements");
+    }
+
+    public static ResultSet getSolarPanelData() throws SQLException {
+        return executeQuery("SELECT * FROM SolarPanelData");
+    }
+
+    public static ResultSet getStateSubsidies() throws SQLException {
+        return executeQuery("SELECT * FROM StateSubsidies");
+    }
+
+    public static ResultSet getSolarIntensity() throws SQLException {
+       return executeQuery("SELECT * FROM SolarIntensity");
+    }
+
+    public static ResultSet getSolarPanelModels() throws SQLException {
+      return executeQuery("SELECT * FROM SolarPanelModels");
+    }
+
+    public static double getSunlightHours(String state, String district) {
+        String query = "SELECT avg_sunlight_hours_per_day FROM SolarIntensity WHERE state_name = ? AND district_name = ?";
+        return fetchDouble(query, state, district);
+    }
+
+    private static ResultSet executeQuery(String query) throws SQLException {
+        Connection con = getConnection();
+        Statement stmt = con.createStatement();
+        return stmt.executeQuery(query);
+    }
+
+    public static ResultSet getStateSubsidyByState(String stateName) throws SQLException {
+    String query = "SELECT * FROM StateSubsidies WHERE state_name = ?";
+    Connection con = getConnection();
+    PreparedStatement ps = con.prepareStatement(query);
+    ps.setString(1, stateName);
+    return ps.executeQuery();
+    }
+
+    private static double fetchDouble(String query, String param1, String param2) {
+        double result = 0.0;
+        try (Connection con = getConnection();
+             PreparedStatement ps = con.prepareStatement(query)) {
+            ps.setString(1, param1);
+            ps.setString(2, param2);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    result = rs.getDouble(1);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
---
+++ Final/PowerPurchaseAgreement.java
@@ -0,0 +1,35 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class PowerPurchaseAgreement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter state name: ");
+        String stateName = scanner.nextLine();
+        
+        try (Connection conn = DatabaseHelper.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(
+                 "SELECT s.state_name, ppa.tariff, ppa.escalation_rate, ppa.contract_duration " +
+                 "FROM PowerPurchaseAgreements ppa " +
+                 "JOIN StateSubsidies s ON ppa.state_id = s.state_id " +
+                 "WHERE s.state_name = ?")) {
+            
+            pstmt.setString(1, stateName);
+            ResultSet rs = pstmt.executeQuery();
+            
+            if (!rs.isBeforeFirst()) {
+                System.out.println("No data found for the entered state.");
+            } else {
+                while (rs.next()) {
+                    System.out.println("State: " + rs.getString("state_name"));
+                    System.out.println("Tariff: $" + rs.getFloat("tariff"));
+                    System.out.println("Escalation Rate: " + rs.getFloat("escalation_rate") + "%");
+                    System.out.println("Contract Duration: " + rs.getInt("contract_duration") + " years");
+                    System.out.println("--------------------------------------");
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
