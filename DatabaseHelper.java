51fc1f501130344a23e05fb02f9b4ae7e5a82081
AshoShibu <ashoshibu@gmail.com>
2025-04-19 15:23:37 +0530

    Update DatabaseHelper.java

--- DatabaseHelper.java
+++ DatabaseHelper.java
@@ -13,3 +13,31 @@ public class DatabaseHelper {
             throw new RuntimeException("MySQL JDBC Driver not found! Add the connector JAR.", e);
         }
     }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static ResultSet getProjects() throws SQLException {
+        return executeQuery("SELECT * FROM Projects");
+    }
+
+    public static ResultSet getPowerPurchaseAgreements() throws SQLException {
+        return executeQuery("SELECT * FROM PowerPurchaseAgreements");
+    }
+
+    public static ResultSet getSolarPanelData() throws SQLException {
+        return executeQuery("SELECT * FROM SolarPanelData");
+    }
+
+    public static ResultSet getStateSubsidies() throws SQLException {
+        return executeQuery("SELECT * FROM StateSubsidies");
+    }
+
+    public static ResultSet getSolarIntensity() throws SQLException {
+       return executeQuery("SELECT * FROM SolarIntensity");
+    }
+
+    public static ResultSet getSolarPanelModels() throws SQLException {
+       return executeQuery("SELECT panel_id, brand, model_name, power_output, cost_per_panel, length, width FROM SolarPanelModels");
+    }
