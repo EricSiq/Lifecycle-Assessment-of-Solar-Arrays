5d132bc8b9918903450f54c3b8eb2a0d506aea87
AshoShibu <ashoshibu@gmail.com>
2025-04-20 17:31:39 +0530

    Update SolarCostCalculator.java

--- SolarCostCalculator.java
+++ SolarCostCalculator.java
@@ -1,1 +1,31 @@
+       } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+        } catch (SQLException e) {
+            System.out.println("Error fetching solar panel models from database.");
+            e.printStackTrace();
+        }
+
+        System.out.println("----------------------------------------------------------------------------------------------");
+    }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+       } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
