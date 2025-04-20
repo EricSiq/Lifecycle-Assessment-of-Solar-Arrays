b6389ef239a94864673e1e8203568e7d48d5ab21
Ameya Tipnis <164353861+Ameya2137@users.noreply.github.com>
2025-04-20 18:20:39 +0530

    Update StateSubsidy.java

--- StateSubsidy.java
+++ StateSubsidy.java
@@ -5,7 +5,26 @@ public class StateSubsidy {
     public static void run(Scanner scanner) { // Accept the scanner as an argument
         System.out.print("Enter state name: ");
         String stateName = scanner.nextLine();  // Use the passed scanner
+  
+        try {
+            ResultSet rs = DatabaseHelper.getStateSubsidies();
+            boolean hasData = false;
 
+            while (rs.next()) {
+                if (rs.getString("state_name").equalsIgnoreCase(stateName)) {
+                    hasData = true;
+                    System.out.println("State: " + rs.getString("state_name"));
+                    System.out.println("Subsidy Percentage: " + rs.getFloat("subsidy_percent") + "%");
+                    System.out.println("--------------------------------------");
+                }
+            }
+
+            if (!hasData) {
+                System.out.println("No subsidy information found for the entered state.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         // Add this line to prompt the user to press Enter before returning to the main menu
         System.out.println("Press Enter to return to the main menu...");
